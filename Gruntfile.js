87ef64883f54dbdb9b5453a0b8f3902591fcff15
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-09-25 12:53:54 -0400

    Add JS concatination

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         jshint: ['Gruntfile.js'],
+
         less: {
             compile: {
                 files: {
                     'build/css/compiled.css': 'public/css/**/*.less'
                 }
             }
+        },
+
+        concat: {
+            js: {
+                files: {
+                    'build/js/bundle.js': 'public/js/**/*.js'
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.registerTask('default', ['jshint']);
 };
